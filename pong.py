bbf7d8fcf12947bffc583354eaf2eb050bf6a85f
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-12-12 10:56:03 +0100

    deplacement et rebond de la balle

--- pong.py
+++ pong.py
@@ -2,19 +2,37 @@ from sense_hat import SenseHat
 import time
 
 sense = SenseHat()
-x=0
-y=0
+sense.clear()
+
+depx = 1
+depy = 1
+x = 7
+y = 3
+z = 0
+
 balle = (255, 255, 255)
 noir = (0, 0, 0)
+raquette = (0, 0, 255)
 
+#affiche un point
 sense.set_pixel(x, y, balle)
 
-while x <7 and y < 7:
+#affichage raquette
+sense.set_pixel(0, z+0, raquette)
+sense.set_pixel(0, z+1, raquette)
+sense.set_pixel(0, z+2, raquette)
 
-  sense.clear()
+#déplacement du point vers coin inférieur droit
+while 1:
 
-  x = x+1
-  y = y+1
+  sense.set_pixel(x, y, noir)
+
+  x -= depx
+  if x == 0 or x == 7:
+    depx *= -1
+  y -= depy
+  if y == 0 or y == 7:
+    depy *= -1
   
   sense.set_pixel(x, y, balle)
   time.sleep(0.2)
