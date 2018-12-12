d624c552180834de15804fafe8bea6dba75411be
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-12-12 10:23:25 +0100

    déplacement du point (balle)

--- pong.py
+++ pong.py
@@ -1,5 +1,20 @@
 from sense_hat import SenseHat
+import time
 
 sense = SenseHat()
+x=0
+y=0
+balle = (255, 255, 255)
+noir = (0, 0, 0)
 
-sense.set_pixel(3, 3, 255, 255, 255)
+sense.set_pixel(x, y, balle)
+
+while x <7 and y < 7:
+
+  sense.clear()
+
+  x = x+1
+  y = y+1
+  
+  sense.set_pixel(x, y, balle)
+  time.sleep(0.2)
